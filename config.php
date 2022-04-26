d67279f3f54ebbb0dd5223bb3944455c5e83ebeb
nicholascsuf <35970284+nicholascsuf@users.noreply.github.com>
2022-04-25 20:41:17 -0700

    Update config.php

--- config.php
+++ config.php
@@ -2,14 +2,14 @@
 // Author: NicholasCSUF
 //CWID:  889345928
 //Date Created: 04-20-2022
-//Copyright © NicholasCSUF. All rights reserved. */
+//Copyright © NicholasCSUF. All rights reserved.
 
 <?php
 session_start();
-$host = "localhost"; /* Host name */
-$user = "web"; /* User */
-$password = "!7eez546ASaqex"; /* Password */
-$dbname = "bookshelf"; /* Database name */
+$host = "localhost"; 
+$user = "web";
+$password = "!7eez546ASaqex"; 
+$dbname = "bookshelf"; 
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 if (!$con) {
