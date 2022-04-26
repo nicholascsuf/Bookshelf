b84966228aa9eb85286f9a41e0cd60f077705bb8
nicholascsuf <35970284+nicholascsuf@users.noreply.github.com>
2022-04-25 20:34:55 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 //Modified by Nicholas Harney 4-20-2022
+// Author: Nicholas J. Harney
+//CWID:  889345928
+//Date Created: 04-20-2022
+//Copyright © Nicholas J. Harney. All rights reserved. */
 
 <?php
 session_start();
