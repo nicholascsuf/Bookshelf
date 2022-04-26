a686c3837b6b02ae318e8df88bdaaf808b87e8ef
nicholascsuf <35970284+nicholascsuf@users.noreply.github.com>
2022-04-25 20:22:52 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
+//Modified by Nicholas Harney 4-20-2022
+
 <?php
 session_start();
 $host = "localhost"; /* Host name */
@@ -6,7 +8,6 @@ $password = "!7eez546ASaqex"; /* Password */
 $dbname = "bookshelf"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
-// Check connection
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
