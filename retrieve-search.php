cb6458b303e2b439bfd73ea070d59671ec25085e
nicholascsuf <35970284+nicholascsuf@users.noreply.github.com>
2022-04-25 20:39:26 -0700

    Update retrieve-search.php

--- retrieve-search.php
+++ retrieve-search.php
@@ -1,3 +1,10 @@
+// Author: NicholasCSUF
+//Date Created: 04-20-2022
+//Copyright © NicholasCSUF All rights reserved. */
+//Last modified by NicholasCSUF 4-25-2022
+
+
+
 <?php
 include 'config.php';
 
